a35d063d76eca32f8ed8b2c6e994e23bd93a771e
Kyle Asaff <kyleasaff@pixleeteam.com>
2020-12-07 18:32:39 -0400

    add eslint

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import SearchScreen from "./src/screens/SearchScreen";
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import SearchScreen from './src/screens/SearchScreen';
 
 const navigator = createStackNavigator(
   {
     Search: SearchScreen,
   },
   {
-    initialRouteName: "Search",
+    initialRouteName: 'Search',
     defaultNavigationOptions: {
-      title: "BusinessSearch",
+      title: 'Business Search',
     },
-  }
+  },
 );
 
 export default createAppContainer(navigator);
---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SearchBar = () => (
+  <View>
+    <Text>Search Screen</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({});
+
+export default SearchBar;
