2a41caa3a906951c4c5e51e7f487f632758d0401
Kyle Asaff <kyleasaff@pixleeteam.com>
2020-12-07 21:24:45 -0400

    eslint things

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
-const SearchBar = () => (
-  <View>
-    <Text>Search Screen</Text>
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => (
+  <View style={styles.backgroundStyle}>
+    <FontAwesome style={styles.iconStyle} name="search" />
+    <TextInput
+      style={styles.inputStyle}
+      placeholder="Search"
+      value={term}
+      onChangeText={onTermChange}
+      autoCapitalize="none"
+      autoCorrect={false}
+      onEndEditing={onTermSubmit}
+    />
   </View>
 );
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: '#F0EEEE',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row',
+    marginTop: 15,
+  },
+  inputStyle: {
+    flex: 1,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15,
+  },
+});
 
 export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,19 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState('');
   return (
     <View>
-      <Text>Search Screen</Text>
+      <SearchBar
+        onTermChange={(value) => setTerm(value)}
+        onTermSubmit={() => console.log(term)}
+      />
+      <Text>
+        Search Screen
+        {term}
+      </Text>
     </View>
   );
 };
